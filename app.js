1a9bcbe776e70a005a3ff97ae08b860dd0909d54
zhangh <740586220@qq.com>
2017-01-06 15:21:24 +0800

    mvc

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const Koa = require('koa');
 
 const app = new Koa();
 
+const fs = require('fs');
+
 //解析原始request请求，然后，把解析后的参数，绑定到ctx.request.body中
 const bodyParser = require('koa-bodyparser'); 
 
 const controller = require('./controllers/controller.js');
+
+let staticFiles = require('./staticFiles.js');
 
 //中间件的顺序很重要
 app.use(async (ctx, next) => {
@@ -19,6 +23,8 @@ app.use(async (ctx, next) => {
 	const useTime = new Date().getTime() - start;
 	console.log(`useTime: ${useTime} ms`);
 })
+
+app.use(staticFiles('/static/', __dirname + '/static'));
 
 //把bodyParser注册到app上 
 app.use(bodyParser());
--- controllers/allController/indexController.js
+++ controllers/allController/indexController.js
@@ -1,7 +1,12 @@
 
+//
+//koa并没有在ctx对象上提供render方法，
+//这里我们假设应该这么使用，
+//我们在编写Controller的时候，最后一步调用ctx.render(view, model)就完成了页面输出
 const url_index = async (ctx, next) => {
-	ctx.response.body = `<h1>index</h1>
-		<p><a href="/login">please signin</a></p>`
+	ctx.render('index.html',{
+		title:'hehe'
+	})
 }
 
 module.exports = {
